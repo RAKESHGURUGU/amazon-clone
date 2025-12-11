56e5c397194569d3a32c74a1d18b8e24f366a49f
RAKESHGURUGU <gurugurakesh9@gmail.com>
2025-12-11 18:54:47 +0530

    This commit added the item photos and description

--- vite-project/src/Items.jsx
+++ vite-project/src/Items.jsx
@@ -4,139 +4,179 @@ function Items() {
     return (
         <section id="items-section">
             <div className="items-container">
-                <h2>Items Component</h2>
+                <h2>Revamp your home in style</h2>
                 <div className="items-grid">
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/BTFGW/PCQC/New/1x/final/186x116_Home_furnishings_2._SY116_CB555624324_.jpg" alt="image" height={120} width={120} />
+                        <p>Cushion covers</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/BTFGW/PCQC/New/1x/final/186x116_Home_decor_1._SY116_CB555624324_.jpg" alt="image" height={120} width={120} />
+                        <p>Figurines, vases</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/BTFGW/PCQC/New/1x/final/186x116_Home_storage_1._SY116_CB555624324_.jpg" alt="image" height={120} width={120} />
+                        <p>Home storage</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/BTFGW/PCQC/New/1x/final/186x116_Home_lighting_2._SY116_CB555624324_.jpg" alt="image" height={120} width={120} />
+                        <p>Lighting solutions</p>
                     </div>
+                    <div id="see-more">See more</div>
                 </div>
             </div>
             <div className="items-container">
-                <h2>Items Component</h2>
+                <h2>Bulk order discounts + Up to 18% GST savings</h2>
                 <div className="items-grid">
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonBusiness/img25/nov/bau/ain/gw/Laptops_186_116_10._SY116_CB777673831_.jpg" alt="image" height={120} width={120} />
+                        <p>Laptops</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonBusiness/img25/nov/bau/ain/gw/KitchenAppliances_186_116_10._SY116_CB777673831_.jpg" alt="image" height={120} width={120} />
+                        <p>K-applications</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonBusiness/img25/nov/bau/ain/gw/OfficeChairs_186_116_10._SY116_CB777673831_.jpg" alt="image" height={120} width={120} />
+                        <p>Furniture</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonBusiness/img25/dec/bau/ain/gw/ABdelivery_186_116._SY116_CB776314701_.jpg" alt="image" height={120} width={120} />
+                        <p>Purchases</p>
                     </div>
+                    <div id="see-more">See more</div>
                 </div>
             </div>
             <div className="items-container">
-                <h2>Items Component</h2>
+                <h2>Appliances for your home | Up to 55% off</h2>
                 <div className="items-grid">
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08RDL6H79._SY116_CB667322346_.jpg" alt="image" height={120} width={120} />
+                        <p>Air conditioners</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08345R1ZW._SY116_CB667322346_.jpg" alt="image" height={120} width={120} />
+                        <p>Refrigerators</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B07G5J5FYP._SY116_CB667322346_.jpg" alt="image" height={120} width={120} />
+                        <p>Microwaves</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/186x116---wm._SY116_CB667322346_.jpg" alt="image" height={120} width={120} />
+                        <p>W-machine</p>
                     </div>
+                    <div id="see-more">See more</div>
                 </div>
             </div>
             <div className="items-container">
-                <h2>Items Component</h2>
+                <h2>Starting ₹149 | Headphones</h2>
                 <div className="items-grid">
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/june/CE/GW/QC/PC/PC_QuadCard_boAt_0.5x._SY116_CB553870684_.jpg" alt="image" height={120} width={120} />
+                        <p>249/- | boAt</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/june/CE/GW/QC/PC/PC_QuadCard_Boult_0.5x._SY116_CB553870684_.jpg" alt="image" height={120} width={120} />
+                        <p>349/- | boult</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/june/CE/GW/QC/PC/PC_QuadCard_Noise_0.5x._SY116_CB553870684_.jpg" alt="image" height={120} width={120} />
+                        <p>649/- | Noise</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/june/CE/MSO/PD3/PC_QuadCard_Zeb_0.5x_1._SY116_CB570220221_.jpg" alt="image" height={120} width={120} />
+                        <p>149/- | Zebronics</p>
                     </div>
+                    <div id="see-more">See more</div>
                 </div>
             </div>
             <div className="items-container">
-                <h2>Items Component</h2>
+                <h2>Starting ₹49 | Deals on home essentials</h2>
                 <div className="items-grid">
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img18/HomeImprovement/harsmisc/2025/March/Wipes_low_res_V1._SY116_CB549138744_.jpg" alt="image" height={120} width={120} />
+                        <p>cleaning supplies</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img18/HomeImprovement/harsmisc/2025/March/Shower_heads_low_res_V1._SY116_CB549138744_.jpg" alt="image" height={120} width={120} />
+                        <p>Bathroom accessories</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img18/HomeImprovement/harsmisc/2025/March/Tools_low_res_V1._SY116_CB549138744_.jpg" alt="image" height={120} width={120} />
+                        <p>Home tools</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img18/HomeImprovement/harsmisc/2025/March/Wallpapers_low_res_V1._SY116_CB549138744_.jpg" alt="image" height={120} width={120} />
+                        <p>Wallpapers</p>
                     </div>
+                    <div id="see-more">See more</div>
                 </div>
             </div>
             <div className="items-container">
-                <h2>Items Component</h2>
+                <h2>Automotive essentials | Up to 60% off</h2>
                 <div className="items-grid">
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/2020/GW/PCQC/Glasscare1X._SY116_CB410830553_.jpg" alt="image" height={120} width={120} />
+                        <p>Cleaning accessories</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/2020/GW/PCQC/Rim_tyrecare1x._SY116_CB410830552_.jpg" alt="image" height={120} width={120} />
+                        <p>Tyre & rim care</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/2020/GW/PCQC/Vega_helmet_186x116._SY116_CB405090404_.jpg" alt="image" height={120} width={120} />
+                        <p>Helmets</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/2020/GW/PCQC/Vaccum1x._SY116_CB410830552_.jpg" alt="image" height={120} width={120} />
+                        <p>Vacuum cleaner</p>
                     </div>
+                    <div id="see-more">See more</div>
                 </div>
             </div>
             <div className="items-container">
-                <h2>Items Component</h2>
+                <h2>Starting ₹199 | Amazon Brands & more</h2>
                 <div className="items-grid">
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_2._SY116_CB567468236_.jpg" alt="image" height={120} width={120} />
+                        <p>Bedsheets</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_3._SY116_CB567468236_.jpg" alt="image" height={120} width={120} />
+                        <p>Curtains</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_4._SY116_CB567468236_.jpg" alt="image" height={120} width={120} />
+                        <p>Ironing board</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_1._SY116_CB567468236_.jpg" alt="image" height={120} width={120} />
+                        <p>Home decor</p>
                     </div>
+                    <div id="see-more">See more</div>
                 </div>
             </div>
             <div className="items-container">
-                <h2>Items Component</h2>
+                <h2>Up to 50% off | Baby care & toys | Amazon Brands</h2>
                 <div className="items-grid">
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/PB/GW/PC/PC_QC_372x232_Life-Style_2_1x._SY116_CB788857785_.jpg" alt="image" height={120} width={120} />
+                        <p>diapers & wipes</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/PB/GW/PC/PC_QC_372x232_Life-Style_1_1x._SY116_CB788857785_.jpg" alt="image" height={120} width={120} />
+                        <p>Ride</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/PB/GW/PC/PC_QC_372x232_Life-Style_3_1x._SY116_CB788857785_.jpg" alt="image" height={120} width={120} />
+                        <p>RC cars</p>
                     </div>
                     <div className="item">
-                        <img src="" alt="image" />
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/PB/GW/PC/PC_QC_372x232_Life-Style_4_1x._SY116_CB788857785_.jpg" alt="image" height={120} width={120} />
+                        <p>Baby safety essentials</p>
                     </div>
+                    <div id="see-more">See more</div>
                 </div>
             </div>
         </section>
